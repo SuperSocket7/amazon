966ff3d9a82d98885bd09b7a524667cec40fe157
猫川七海 <mail@nekokawa.net>
2023-01-08 20:08:09 +0900

    Initial commit

---
+++ extension.js
@@ -0,0 +1,6 @@
+const address = document.getElementById('nav-global-location-slot'); 
+const name = document.getElementById('nav-link-accountList-nav-line-1');
+const address2 = document.getElementById('contextualIngressPtLabel');
+address.remove();
+name.remove();
+address2.remove();
